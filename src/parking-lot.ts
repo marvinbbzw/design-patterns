3776ac244a7f510b33079c2f60d9a282fc817e58
Marvin Eggerschwiler <marvin_eggerschwiler@sluz.ch>
2025-05-23 15:35:43 +0200

    Parkinlot erweitert

--- src/parking-lot.ts
+++ src/parking-lot.ts
@@ -1,32 +1,53 @@
-export class ParkingLot {
-  public occupied: number = 0;
+import { Publisher, Subscriber, Event } from "./observer.js";
+
+export class ParkingLot implements Publisher {
+  private occupied = 0;
+  private subscribers: Subscriber[] = [];
 
   constructor(
     public name: string,
     public capacity: number,
   ) {}
 
-  enter() {
-    if (!this.isFull()) {
-      this.occupied++;
-    } else {
-      throw new Error(`the parking lot is full`);
-    }
+  public enter(): void {
+    if (this.isFull()) throw new Error("Parking lot full");
+    this.occupied++;
+    this.notify({
+      lotName: this.name,
+      occupied: this.occupied,
+      capacity: this.capacity,
+      type: "enter",
+    });
   }
 
-  exit() {
-    if (!this.isEmpty()) {
-      this.occupied--;
-    } else {
-      throw new Error(`the parking lot is empty`);
-    }
+  public exit(): void {
+    if (this.isEmpty()) throw new Error("Parking lot empty");
+    this.occupied--;
+    this.notify({
+      lotName: this.name,
+      occupied: this.occupied,
+      capacity: this.capacity,
+      type: "exit",
+    });
   }
 
-  isFull() {
-    return this.occupied == this.capacity;
+  public isFull(): boolean {
+    return this.occupied === this.capacity;
   }
 
-  isEmpty() {
-    return this.occupied == 0;
+  public isEmpty(): boolean {
+    return this.occupied === 0;
+  }
+
+  public subscribe(sub: Subscriber): void {
+    this.subscribers.push(sub);
+  }
+
+  public unsubscribe(sub: Subscriber): void {
+    this.subscribers = this.subscribers.filter((s) => s !== sub);
+  }
+
+  public notify(event: Event): void {
+    this.subscribers.forEach((sub) => sub.update(event));
   }
 }
